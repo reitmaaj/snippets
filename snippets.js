28805e508ce7c2af26c9b4509b4bbc86d8c813f5
reitmaaj <66722817+reitmaaj@users.noreply.github.com>
2025-10-27 11:43:34 +0200

    yield elapsed time from `animate`, not abstract progress

--- snippets.js
+++ snippets.js
@@ -1,10 +1,9 @@
 
 async function* animate(duration) {
-    let progress;
     const start = performance.now();
+    let elapsed;
     do {
-        yield progress = Math.min((performance.now() - start) / duration, 1.0);
         await new Promise(requestAnimationFrame);
-    } while (progress < 1.0);
+        yield elapsed = Math.min(performance.now() - start, duration);
+    } while (elapsed < duration);
 }
-

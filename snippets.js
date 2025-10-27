eb94e36801c57079c11e37be9cf02c23cf911089
reitmaaj <66722817+reitmaaj@users.noreply.github.com>
2025-10-27 16:22:07 +0200

    add rudimentary easing

--- snippets.js
+++ snippets.js
@@ -7,3 +7,14 @@ async function* animate(duration) {
         yield elapsed = Math.min(performance.now() - start, duration);
     } while (elapsed < duration);
 }
+
+function linear(progress) {
+    return progress;
+}
+
+function createEasing({from, to, duration, ease = linear}) {
+    return function easing(timestamp) {
+        progress = ease(timestamp / duration);
+        return progress * (to - from) + from;
+    }
+}

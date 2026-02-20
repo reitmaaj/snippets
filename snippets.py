65ac1c3d90deee99c8f7a1cb0b469aaa6d9ecc69
reitmaaj <66722817+reitmaaj@users.noreply.github.com>
2026-02-20 18:15:26 +0200

    Create _create_conf function for loading configurations
    Refactor configuration loading into a separate function.

--- snippets.py
+++ snippets.py
@@ -318,7 +318,6 @@ def awaitable(fn):
 
 
 # configuration
-
 import os
 import sys
 import json
@@ -329,18 +328,23 @@ import pathlib
 def _object_hook(value):
     return types.SimpleNamespace(**value)
 
+def _resolve_default_conf_path():
+    return pathlib.Path(__file__).parent / 'default.conf.json'
+
 def _resolve_conf_path():
     for conf_path in (os.getenv('CONF_PATH'), 'conf.json',):
         if not conf_path is None:
             return pathlib.Path(conf_path)
 
-default_conf_path   = pathlib.Path(__file__).parent / 'default.conf.json'
-conf_path           = pathlib.Path(_resolve_conf_path())
-with (default_conf_path.open() as df, conf_path.open() as cf):
-    default_conf    = json.load(df, object_hook=_object_hook)
-    conf            = json.load(cf, object_hook=_object_hook)
-                    
-sys.modules[__name__] = types.SimpleNamespace(**{**default_conf.__dict__,
-                                                 **conf.__dict__,})
+def _create_conf(default_conf_path, conf_path):
+    with (default_conf_path.open() as df, conf_path.open() as cf):
+        default_conf = json.load(df, object_hook=_object_hook)
+        conf = json.load(cf, object_hook=_object_hook)
+        return types.SimpleNamespace(**{**default_conf.__dict__,
+                                        **conf.__dict__,})
+
+
+sys.modules[__name__] = _create_conf(_resolve_default_conf_path(),
+                                     _resolve_conf_path())
 
 
